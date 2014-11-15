4c7eb04d345e6f2d4b5b86fa6b75b9b94ba91e46
Deepesh Shetty <shetty.deepesh412@gmail.com>
2014-11-15 22:00:44 +0530

    Just a commit

---
+++ js/app/example.js
@@ -0,0 +1,28 @@
+var http = require('http');
+http.createServer(function (req, res) {
+    var MongoClient = require('mongodb').MongoClient;
+
+    MongoClient.connect('mongodb://127.0.0.1:27001/zivhack',function(err, db){
+
+        if(err) throw err;
+
+        var query={"gender_id":"gen1"}
+
+        db.collection('category').find(query).toArray(function(err, doc){
+            //Print the result
+            console.dir(doc);
+
+            res.writeHead(200, {'Content-Type': 'text/plain'});
+            res.end(JSON.stringify(doc));
+            // Cloese the db
+            db.close();
+        });
+
+//Declare Sucess
+
+        console.dir("Called FindOne");
+    });
+
+
+}).listen(1337, '127.0.0.1');
+console.log('Server running at http://127.0.0.1:1337/');
